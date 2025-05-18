dd97be0269c0fdea505759cacee75342146d057c
CC11001100 <CC11001100@qq.com>
2025-05-18 19:26:36 +0800

    feat: 添加油猴API的TypeScript类型定义支持
    1. 安装@types/tampermonkey包提供官方类型定义\n2. 创建src/types/tampermonkey.d.ts实现自定义类型扩展\n3. 更新tsconfig.json，添加typeRoots配置\n4. 更新userscript-headers.js，添加必要的@grant声明

--- userscript-headers.js
+++ userscript-headers.js
@@ -7,4 +7,17 @@
 // @author       ${author}
 // @match        *://*/*
 // @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_xmlhttpRequest
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.deleteValue
+// @grant        GM.listValues
+// @grant        GM.registerMenuCommand
+// @grant        GM.xmlHttpRequest
 // ==/UserScript==
